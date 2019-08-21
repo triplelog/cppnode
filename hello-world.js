4932431ede81f341c6801df860fb936eb0a5bd8f
triplelog <robmwilc@gmail.com>
2019-08-21 12:18:48 -0400

    f

--- hello-world.js
+++ hello-world.js
@@ -11,16 +11,10 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-  	var d = new Date();
-	var n = d.getTime();
-	console.log(n);
+  	
   	message += '\n';
 	message = message.replace(/\|/g, '\n');
 	message = message.replace(/\\n/g, '\r\n');
-	
-	var d = new Date();
-	var n = d.getTime();
-	console.log(n);
 	
 	try {
 	  fs.unlinkSync('ff.csv');
@@ -29,28 +23,17 @@ wss.on('connection', function connection(ws) {
 	  //console.error(err);
 	}
 	
-	var d = new Date();
-	var n = d.getTime();
-	console.log(n);
-	
 	fs.appendFile("newtesttxt.txt", message, (err) => {
   	
 	});
 	
-	var d = new Date();
-	var n = d.getTime();
-	console.log(n);
-	
-	setTimeout(intervalFunc,10, ws);
+	setTimeout(intervalFunc,5, ws);
 	
 	
   });
 });
 
 function intervalFunc(ws) {
-		var d = new Date();
-		var n = d.getTime();
-		console.log('fn, ',n);
 		fs.stat('ff.csv', function(err, stats) {
 			if (!err) {
 				if (stats.isFile() && stats.size > 16) {
