7e84b5832e262cc4dc3ffc39fdebfb60f57a3c7d
triplelog <robmwilc@gmail.com>
2019-08-21 09:40:13 -0400

    f

--- hello-world.js
+++ hello-world.js
@@ -6,30 +6,14 @@ var app = express();
 app.use('/static',express.static('static'));
 var serverStatic = app.listen(12312);
 
-/*
-const hostname = '127.0.0.1';
-const port = 12312;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-	message = "ff.csv,0,10,sort,HR\n";
-	fs.appendFile("../go/src/cppsvserver/newtesttxt.txt", message, (err) => {
-  	if (err) console.log(err);
-  	console.log("Successfully Written to File.");
+
+	fs.appendFile("newtesttxt.txt", message, (err) => {
+  	
 	});
   });
 });
