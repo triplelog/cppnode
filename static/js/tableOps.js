394adb8251b449d3ee11aed5e738967e76181ee4
triplelog <robmwilc@gmail.com>
2019-08-22 12:49:05 -0400

    f

--- static/js/tableOps.js
+++ static/js/tableOps.js
@@ -1,7 +1,7 @@
 dragula([document.getElementById("moveColumns")], {
   removeOnSpill: true,
 }).on('drop', function (el, target, source, sibling) {
-    var mymessage = "ff.csv,0,10,display,"+el.id.substring(7,);
+    var mymessage = filen+",0,10,display,"+el.id.substring(7,);
     if (sibling) {
     	mymessage += "@"+sibling.id.substring(7,);
     }
