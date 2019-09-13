1a8e545aa8226d63adc866fc6ebed54c85207143
triplelog <robmwilc@gmail.com>
2019-09-12 22:10:04 -0400

    f

--- static/wasm/uploadworker.js
+++ static/wasm/uploadworker.js
@@ -2,6 +2,31 @@
 
 self.addEventListener('message', function(e) {
     var data=e.data;
+    var Module = {
+		preRun: [],
+		postRun: [],
+		setStatus: function(text) {
+		  if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
+		  if (text === Module.setStatus.last.text) return;
+		  var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+		  var now = Date.now();
+		  if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
+		  Module.setStatus.last.time = now;
+		  Module.setStatus.last.text = text;
+		  if (m) {
+			text = m[1];
+		  } else {
+		  }
+		},
+		totalDependencies: 0,
+		monitorRunDependencies: function(left) {
+		  this.totalDependencies = Math.max(this.totalDependencies, left);
+		  Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+		}
+	};
+	Module.setStatus('Downloading...');
+	importScripts('datatype.js');
+	
     try {
         var readerP = new FileReader();
 	
@@ -30,30 +55,7 @@ self.addEventListener('message', function(e) {
    }
 }, false);
 
-var Module = {
-	preRun: [],
-	postRun: [],
-	setStatus: function(text) {
-	  if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
-	  if (text === Module.setStatus.last.text) return;
-	  var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
-	  var now = Date.now();
-	  if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
-	  Module.setStatus.last.time = now;
-	  Module.setStatus.last.text = text;
-	  if (m) {
-		text = m[1];
-	  } else {
-	  }
-	},
-	totalDependencies: 0,
-	monitorRunDependencies: function(left) {
-	  this.totalDependencies = Math.max(this.totalDependencies, left);
-	  Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
-	}
-};
-Module.setStatus('Downloading...');
-importScripts('datatype.js');
+
 
 
 
