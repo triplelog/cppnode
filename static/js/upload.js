6743e691f1c35527ad860d0440c7c56c52874d9c
triplelog <robmwilc@gmail.com>
2019-09-12 20:18:59 -0400

    f

--- static/js/upload.js
+++ static/js/upload.js
@@ -8,7 +8,7 @@ document.querySelector('#to-compress').addEventListener('change', function(inp) 
 	syncWorker.onmessage = function(e) {
 		ctypestr = toTable(e.data.result);
 		//if (filen != ""){createConfirmForm();}
-		setTimeout(fullCompression,100,ffile);
+		setTimeout(fullCompression,1000,ffile);
 	};
 	
 	
