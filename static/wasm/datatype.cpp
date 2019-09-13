90a35519650671e644127cc05f3785f34872d8a7
triplelog <robmwilc@gmail.com>
2019-09-13 11:03:18 -0400

    f

--- static/wasm/datatype.cpp
+++ static/wasm/datatype.cpp
@@ -48,13 +48,12 @@ char* trim(char* x) {
 char* getType(char* x) {
 	char* out[10]; int i;
 	while (*x){
-		//if (i == 0 && *x == ' '){}
-		//else if (i == 0 && *x == '\t'){}
-		//else {out[i] = *x; i++;}
+		if (i == 0 && *x == ' '){}
+		else if (i == 0 && *x == '\t'){}
+		else {out[i] = *x; i++;}
 		*x++;
 	}
-	out[0] = 'a';
-	out[1] = '\0';
+	out[i] = '\0';
 	/*
     input_str = input_str.trim().toLowerCase();
 	head_str = head_str.trim().toLowerCase();
