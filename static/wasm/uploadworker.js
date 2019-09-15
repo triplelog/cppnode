66962df21b3275feb8ebb21ede610662c71274d5
triplelog <robmwilc@gmail.com>
2019-09-15 09:18:30 -0400

    f

--- static/wasm/uploadworker.js
+++ static/wasm/uploadworker.js
@@ -88,7 +88,7 @@ self.addEventListener('message', function(e) {
 			});
 		
 		}
-		readerP.readAsArrayBuffer(data.slice(0,10000).concat(data.slice(data.length-10000,data.length)));
+		readerP.readAsArrayBuffer(data.slice(0,20000));
 
    } catch(e){
         postMessage({
