2ac7598726dd7f8f60c0d566d843fc56457557b9
triplelog <robmwilc@gmail.com>
2019-09-14 21:47:46 -0400

    f

--- static/js/upload.js
+++ static/js/upload.js
@@ -131,7 +131,7 @@ function toTable(input_str,ctypestr){
 		var th = headCells[i];
 		var tdiv = th.querySelector("div");
 		var tspan = tdiv.querySelector("span");
-		tspan.textContent += ' (as '+cheaders[i]+')';
+		tspan.innerHTML += ' (as '+cheaders[i]+')';
 	}
 
 	return ctypestr;
