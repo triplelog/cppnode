dc733f833ee716112fd9a00b6dba6f556762f6d5
triplelog <robmwilc@gmail.com>
2019-10-01 22:21:04 -0400

    f

--- static/wasm/uploadworker.js
+++ static/wasm/uploadworker.js
@@ -61,9 +61,7 @@ self.addEventListener('message', function(e) {
 				var isdata = 0; var isstring = 0;
 				for (var ii=1; ii<parsedstr.length; ii++ ) {
 					if (i<parsedstr[ii].length){
-						console.log(parsedstr[ii][i]);
-						var dtype = get_type(parsedstr[ii][i]);
-						console.log('done');
+						var dtype = get_type(parsedstr[ii][i].substring(0,19));
 						if (dtype == "string") {
 							isstring++;
 						}
