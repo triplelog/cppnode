0104a24667b2ebd97a211629cdfd91b33220c132
triplelog <robmwilc@gmail.com>
2019-12-05 23:07:56 -0500

    nm

--- texdn-server.js
+++ texdn-server.js
@@ -45,7 +45,7 @@ https.createServer(options, function(req, res) {
 			fs.writeFile("texdnLatex/newtest.tex", qs.parse(data).latexArea, function (err) {
 				var runtime = process.hrtime(start) // we also check how much time has passed
 				console.info('Execution time (hr): %ds %dms', runtime[0], runtime[1] / 1000000);
-				exec('latex newtest2.tex');
+				exec('time latex newtest2.tex');
 				console.log('Executing');
 			});
 			res.write('avvv'); //write a response to the client
