e28852a4fecbc0aac153439c5b0ae71f051b98c4
triplelog <robmwilc@gmail.com>
2019-12-05 22:30:09 -0500

    nm

--- texdn-server.js
+++ texdn-server.js
@@ -1,7 +1,8 @@
 
 const https = require('https');
 var fs = require("fs");
-var myParser = require("body-parser");
+//var myParser = require("body-parser");
+var qs = require('querystring');
 
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/tabdn.com/privkey.pem'),
@@ -26,23 +27,25 @@ server1.listen(12312);
 
 https.createServer(options, function(req, res) {
 	if (req.url == "/createchart"){
-		var data = [];
+		var data = '';
+
+        req.on('data', function (chunk) {
+            data += chunk;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (data.length > 1e6)
+                req.connection.destroy();
+        });
 
 		// when we get data we want to store it in memory
 		req.on('end', () => {
-			
-			res.write('asd'); //write a response to the client
+			console.log(qs.parse(data));
+			res.write('avvv'); //write a response to the client
 			res.end();
 		});
 	
-		req.on('data', chunk => {
-			console.log(chunk);
-			//console.log(chunk.length);
-			//res.write(chunk.length);
-			data.push(chunk);
 
-		// below we process the full data
-		});
     }
 
     else if (req.url.substring(0,9) == "/savefile"){
