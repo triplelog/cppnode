80dde3c00e89b4a64d29ea4bee7b6c38644e3c34
triplelog <robmwilc@gmail.com>
2019-12-05 23:12:26 -0500

    nm

--- texdn-server.js
+++ texdn-server.js
@@ -45,7 +45,7 @@ https.createServer(options, function(req, res) {
 			fs.writeFile("texdnLatex/newtest.tex", qs.parse(data).latexArea, function (err) {
 				var runtime = process.hrtime(start) // we also check how much time has passed
 				console.info('Execution time (hr): %ds %dms', runtime[0], runtime[1] / 1000000);
-				exec('latex texdnLatex/newtest2.tex >> output1.txt');
+				exec('latex texdnLatex/newtest2.tex -output-directory=texdnLatex | dvisvgm --clipjoin texdnLatex/newtest2.dvi --font-format=woff >> output2.txt');
 				console.log('Executing');
 			});
 			res.write('avvv'); //write a response to the client
