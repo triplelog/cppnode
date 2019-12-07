2b9720ca8a05f57d218bd7dce04d92be876a56cf
triplelog <robmwilc@gmail.com>
2019-12-07 00:27:55 -0500

    nm

--- static/charts/pythonscript.py
+++ static/charts/pythonscript.py
@@ -26,7 +26,7 @@ for line in fread:
     		i+=1
     	fwrite.write(newline)
     elif line.find('</svg>') > -1:
-    	fwrite.write('<script>function barClicked(e) {alert(e.id);}</script>\n</svg>')
+    	fwrite.write('<script>function barClicked(e) {e.fill = "red";}</script>\n</svg>')
     else:
     	fwrite.write(line)
 
