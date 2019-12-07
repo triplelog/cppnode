b41ed99b9e80ef4dd579d4b16c7819f2482e2300
triplelog <robmwilc@gmail.com>
2019-12-07 18:17:31 -0500

    nm

--- texdn-server.js
+++ texdn-server.js
@@ -175,13 +175,13 @@ const lineChart = new chartXkcd.Line(document.querySelector('#xkcdSvg'), {
   xLabel: 'Month', // optional
   yLabel: '$ Dollars', // optional
   data: {
-    labels: replacexarray,
+    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
     datasets: [{
       label: 'Plan',
-      data: replaceyarray,
+      data: [30, 70, 200, 300, 500, 800, 1500, 2900, 5000, 8000],
     }, {
       label: 'Reality',
-      data: replaceyyarray,
+      data: [0, 1, 30, 70, 80, 100, 50, 80, 40, 150],
     }],
   },
   options: { // optional
